567ffc038869270850cca936c7a1ca48212ea70f
S7RL7SAbsPfLB8tS <81462762+S7RL7SAbsPfLB8tS@users.noreply.github.com>
2021-11-04 01:26:24 +0000

    💀

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
-var text = new TextDecoder("utf-8");
+const text = new TextDecoder('utf-8')
 
-WebSocket.prototype.original = WebSocket.prototype.send;
-WebSocket.prototype.send = function(data) {
-    if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
-        if (text.decode(data).includes("self_deaf")) {
-            console.log("found mute/deafen");
-            data = data.replace('"self_mute":false', 'NiceOneDiscord');
-            console.log("faked - borkgang.com");
-        }
-    }
-    WebSocket.prototype.original.apply(this, [data]);
+WebSocket.prototype.original = WebSocket.prototype.send
+
+WebSocket.prototype.send = data => {
+  if (
+    Object.prototype.toString.call(data) === '[object ArrayBuffer]' &&
+    text.decode(data).includes('self_deaf')
+  )
+    data = data.replace('"self_mute":false', 'ez')
+
+  WebSocket.prototype.original.apply(this, [data])
 }
